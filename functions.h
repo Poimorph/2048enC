99070aa3d1f12eaf1e8393a7c9e4db04b8933599
Bryorny <95055291+Bryorny@users.noreply.github.com>
2023-11-14 10:36:59 +0100

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+int modifiy_value_position(int, int);
+int compare(int, int);
+
+int add_case(int, int);
+void modify_score(int, int);
+
+int fusion(int, int);
+
+
+#endif //FUNCTIONS_H
