395ed9c27125988dba80f2d6b48acdabe8c29915
Bryorny <95055291+Bryorny@users.noreply.github.com>
2023-11-14 10:50:43 +0100

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "functions.h"
+#include <time.h>
+#include <stdlib.h>
 
 int compare(int value1, int value2){
   if (value1 == value2){
@@ -9,4 +11,16 @@ int compare(int value1, int value2){
 
 }
 
-int add_case(
+void add_case(int Tab[][], int length){
+  srand(time(0));
+
+  int x = (rand()%length);
+  int y = (rend()%length);
+
+  while (Tab[y][x] != null){
+    x = (rand()%length);
+    y = (rand()%length);
+  }
+  Tab[y][x] = 2;
+}
+
