8e8df195156ab4bcb5a2b2c9c55acb918464cbf5
Wintoza <122921875+Wintoza@users.noreply.github.com>
2023-11-21 11:50:54 +0100

    Update Jeu.c
    fonction Fusion

--- Jeu.c
+++ Jeu.c
@@ -4,8 +4,19 @@
 #include <windows.h>
 
 
+int Score(int n,int T[n][n]){
+    int score = 0 ;
+    for (int i =0;i<n;i++){
+        for (int y =0;y<n;y++){
+            score += T[i][y];
+        }
+    }
+    return score;
+}
+
 void Affiche(int n, int T[n][n]){
     system("cls");
+    printf("Score : %d", Score(n,T));
     for (int i =0; i<n; i++ ){
         for (int y =0; y<n; y++ ) {
             if (y == 0) {
@@ -14,13 +25,7 @@ void Affiche(int n, int T[n][n]){
             printf("%d ", T[i][y]);
         }}}
 
-int Compare(int i1, int i2 ){
-    if (i1 == i2){
-        return i1*2;
-    }else {
-        return 0;
-    }
-}
+
 void Apparition(int n,int T[n][n]){
     int b,a;
     do{
@@ -34,21 +39,34 @@ void Apparition(int n,int T[n][n]){
     }
 
 }
-
-
 int PremierCaseLibreUp (int n ,int T[n][n], int i){
     for (int y = 0; y<n ; y++){
         if (T[y][i]== 0) {
             return y;
-        }}}
-
+        }
+    }
+}
 int PremierCaseLibreDown (int n ,int T[n][n], int i){
-    for (int y = n; y>0 ; y -- ){
+    for (int y = n-1; y>-1 ; y -- ){
         if (T[y][i]== 0) {
             return y;
-        }}}
-
-
+        }
+    }
+}
+int PremierCaseLibreLeft (int n ,int T[n][n], int i){
+    for (int y = 0; y<n ; y ++ ){
+        if (T[i][y]== 0) {
+            return y;
+        }
+    }
+}a
+int PremierCaseLibreRight (int n ,int T[n][n], int i){
+    for (int y = n-1; y > -1 ; y -- ){
+        if (T[i][y] == 0) {
+            return y;
+        }
+    }
+}
 
 void MoveUp(int n,int T[n][n]){
     for (int i = 0; i<n;i++){
@@ -66,7 +84,7 @@ void MoveUp(int n,int T[n][n]){
 
 void MoveDown(int n,int T[n][n]){
     for (int i = 0; i<n;i++){
-        for (int y = n ; y>-1; y--){
+        for (int y = n-1 ; y>-1; y--){
             if (T[y][i] !=  0 ){
                 int Case = PremierCaseLibreDown(n, T,i);
                 if (Case > y ){
@@ -77,20 +95,89 @@ void MoveDown(int n,int T[n][n]){
         }
     }
 }
+void MoveLeft(int n,int T[n][n]){
+    for (int i = 0; i<n;i++){
+        for (int y = 0 ; y<n; y++){
+            if (T[i][y] !=  0 ){
+                int Case = PremierCaseLibreLeft(n, T,i);
+                if (Case < y ){
+                    T[i][Case] = T[i][y];
+                    T[i][y] = 0 ;
+                }
+            }
+        }
+    }
+}
+void MoveRight(int n,int T[n][n]){
+    for (int i = 0; i<n;i++){
+        for (int y = n-1 ; y>-1; y--){
+            if (T[i][y] !=  0 ){
+                int Case = PremierCaseLibreRight(n, T,i);
+                if (Case > y ){
+                    T[i][Case] = T[i][y];
+                    T[i][y] = 0 ;
+                }
+            }
+        }
+    }
+}
+
+void FusionUp(int n, int T[n][n]){
+    for( int i=0 ;i<n; i++){
+        for (int y = 0; y<n-1;y++){
+            if (T[y][i] == T[y+1][i]){
+                T[y][i] = T[y][i]*2;
+                T[y+1][i] = 0 ;
+                MoveUp(n,T);
+            }
+        }
+    }
+}
+void FusionDown(int n, int T[n][n]){
+    for( int i=0 ;i<n; i++){
+        for (int y = n-1; y>-1;y--){
+            if (T[y][i] == T[y-1][i]){
+                T[y][i] = T[y][i]*2;
+                T[y-1][i] = 0 ;
+                MoveDown(n,T);
+            }
+        }
+    }
+}
+void FusionLeft(int n, int T[n][n]){
+    for( int i=0 ;i<n; i++){
+        for (int y = 0; y<n-1;y++){
+            if (T[i][y] == T[i][y+1]){
+                T[i][y] = T[i][y]*2;
+                T[i][y+1] = 0 ;
+                MoveLeft(n,T);
+            }
+        }
+    }
+}
+void FusionRight(int n, int T[n][n]){
+    for( int i=0 ;i<n; i++){
+        for (int y = n-1; y>-1;y--){
+            if (T[i][y] == T[i][y-1]){
+                T[i][y] = T[i][y]*2;
+                T[i][y-1] = 0 ;
+                MoveRight(n,T);
+            }
+        }
+    }
+}
+
+
+void Fini(int n, int T[n][n]){
+    
+}
 
 int main() {
    int T[4][4]={0,2,0,0,2,2,0,0,0,0,2,0,2,2,2,0};
     srand( time( NULL ) );
 
-    Affiche(4,T);
-    system("pause");
 
-    MoveUp(4,T);
-    Affiche(4,T);
-    system("pause");
-    MoveDown(4,T);
-    Affiche(4,T);
+
 
     return 0;
 }
-
