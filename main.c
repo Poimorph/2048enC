daad002e65483ccdf6d70a2d6fb9c2377d3db998
Suhayb <98946068+Poimurgh@users.noreply.github.com>
2023-11-27 17:53:06 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
+#include "functions.h"
+#include <stdlib.h>
 
 int main(){
-
-
-
-  return 0;
-  
-}
+    int T[4][4]={0,8,0,0,
+                 2,2,0,4,
+                 0,0,2,0,
+                 2,16,2,0};
+    affiche(4,T);
+    system("pause");
+    fusion(4,T,'l');
+    affiche(4,T);
+    printf("\n");
+    move(4,T,'l');
+    affiche(4,T);
+    system("pause");
+    fusion(4,T,'r');
+    affiche(4,T);
+    system("pause");
+    fusion(4,T,'u');
+    affiche(4,T);
+    system("pause");
+    fusion(4,T,'d');
+    affiche(4,T);}
