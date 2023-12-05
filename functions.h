98bfc2c5692249349e6dcd40ce6611fb99c5b88a
Bryorny <95055291+Bryorny@users.noreply.github.com>
2023-12-05 11:58:22 +0100

    Update functions.h

--- functions.h
+++ functions.h
@@ -8,6 +8,9 @@ void move(int n, int T[n][n],char sens);
 void add_case(int length,int Tab[length][length], int *score);
 void jeu(int n, int mode);
 void creationTab(int n,int T[n][n]);
-
+void duo(int n, int T1[n][n], int T2[n][n], int* score);
+void normal(int n, int T1[n][n], int* score);
+void affiche_duo(int n, int T1[n][n], int T2[n][n], int* score);
+void puzzle(int* score);
 
 #endif //FUNCTIONS_H
