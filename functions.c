394ad212417ba8f363e4748053d252325a22908d
Bryorny <95055291+Bryorny@users.noreply.github.com>
2023-12-30 21:25:50 +0100

    Update functions.c

--- functions.c
+++ functions.c
@@ -7,21 +7,22 @@
 #include <ctype.h>
 
 void affiche(int n, int T[n][n],int*Score){
+    /* La fonction affiche, print dans la console le terrain de jeu du 2048*/
     printf("Score Actuel : %d\n",*Score);
-    for (int i =0; i<n; i++ ){
+    for (int i =0; i<n; i++ ){// Nous parcourons le tableaux
         for (int y =0; y<n; y++ ) {
-            if (T[i][y] == -1){
+            if (T[i][y] == -1){// Nous avons décidé de la lettre X sera représenté par un -1 dans le tableaux.
                 printf("%-7c", 'X');
-            }else {
+            } else {
                 printf("%-7d", T[i][y]);
             }
-
         }
         printf("\n");
     }
 }
 
 void affiche_duo(int n, int T1[n][n], int T2[n][n], int* score){
+    // Meme principe que pour la fonction affiche() seulement ici nous affichons deux tableaux côte à côte
     printf("Score Actuel ! %d\n", *score);
     for (int i = 0; i < n; i++){
         for (int y = 0; y < n; y++){
@@ -36,6 +37,7 @@ void affiche_duo(int n, int T1[n][n], int T2[n][n], int* score){
 }
 
 int compare(int value1, int value2){
+    /* Fonction simple de comparaison de deux valeur */
     if (value1 == value2){
         return 1;
     }
@@ -44,19 +46,24 @@ int compare(int value1, int value2){
     }}
 
 void add_case(int length,int Tab[length][length], int *score){
+
+    /* Ajoute une case de valeur compris entre {2, 4}*/
+
+    // On prend des valeurs aléatoires pour les coordonnées X et Y
     int x = (rand()%length);
     int y = (rand()%length);
 
-    while (Tab[y][x] != 0){
+    while (Tab[y][x] != 0){ // On cherche pour quelles coordonnées X et Y la case peut être ajouté
         x = (rand()%length);
         y = (rand()%length);
     }
-    int newValue=(rand()%2+1)*2;
+    int newValue=(rand()%2+1)*2; // Nous définissons la valeur de la case ajouté
     Tab[y][x] = newValue;
-    *score += newValue;
+    *score += newValue; // Nous augmentons le score
 }
 
 void move(int n, int T[n][n],char sens){
+    //Dirige toutes las case non nulles dans la direction souhaité
     int caseLibre[n][2], traker,index;
     switch (sens) {
         case 'd':
@@ -159,23 +166,27 @@ void move(int n, int T[n][n],char sens){
 }
 
 void fusion(int n, int Tab[n][n], char sensRotation){
-    /*    On prendra d pour de gauche à droite
+    /*    On prendra :
+          d pour de gauche à droite
           q pour de droit à gauche
           s pour de haut en bas
           z pour de bas en haut*/
 
     switch(sensRotation){
 
+        // Le principe du code est le même pour les 4 possibilité du switch
+
         case 'd':
 
             for (int x = 0; x < n; x++){
-                for (int y = n-1; y >=0; y--){
+                for (int y = n-1; y >=0; y--){ // Nous parcourons le tableaux de droite à gauche
                     for(int j=y-1;j>=0;j--){
-                        if (compare(Tab[x][y], Tab[x][j])){
-                            Tab[x][y] = Tab[x][y]*2;
-                            Tab[x][j] = 0;
-                            j=-1;}
-                        else if (Tab[x][j]!=0){
+                        if (compare(Tab[x][y], Tab[x][j])){ // J est une variable qui vérifie toujours y-j = 1
+                            Tab[x][y] = Tab[x][y]*2;                        //donc nous comparons un élément avec son précéent dans la liste
+                            Tab[x][j] = 0;                                  //pour vérifier si ils peuvent être fusionnées
+                            j=-1;
+                        }
+                        else if (Tab[x][j]!=0){ // Si la valeur précédentes à y n'est pas égale à y ni à 0 alors on sort de la boucle.
                             j=-1;
                         }
                     }
@@ -237,6 +248,7 @@ void fusion(int n, int Tab[n][n], char sensRotation){
 
     }}
 void creationTab(int n,int T[n][n]){
+    /*Créer un tableaux à double dimensions vide*/
     for (int i=0;i<n;i++){
         for (int j=0;j<n;j++){
             T[i][j]=0;
@@ -245,45 +257,50 @@ void creationTab(int n,int T[n][n]){
 }
 
 void duo(int n, int T1[n][n], int T2[n][n], int *score){
-    int a = 1;
-    char dir;
+    /* Fonction principale du mode duo*/
+    int jouer = 1;
+    char direction;
+    //Préparation du mode de jeu
     creationTab(n, T1);
     creationTab(n, T2);
     add_case(n, T1, score);
     add_case(n, T2, score);
     affiche_duo(n, T1, T2, score);
-    while (a) {
-        printf("dans quel direction voulez vous aller ? (q/d/z/s) ou 'a' pour quitter\n");
-        scanf("%c", &dir);
+    while (jouer) {
+        printf("Dans quel direction voulez-vous aller ? (q/d/z/s) ou 'a' pour quitter\n");
+        scanf("%c", &direction);// Récupération du choix du joueur
         fflush(stdin);
-        tolower(dir);
-        if (dir == 'a') { a = 0; }
+        tolower(direction);
+        if (direction == 'a') {
+            jouer = 0; //Si le joueur décide de mettre fin à sa partie
+        }
         else {
-            fusion(n, T1, dir);
-            fusion(n, T2, dir);
-            move(n, T1, dir);
-            move(n, T2, dir);
-            add_case(n, T1, score);
+            fusion(n, T1, direction);// Opération de fusion dans les deux tableaux
+            fusion(n, T2, direction);
+            move(n, T1, direction);// Opération de mouvement dans les deux tableaux
+            move(n, T2, direction);
+            add_case(n, T1, score);// Ajout de case aléatoires dans les deux tableaux
             add_case(n, T2, score);
-            affiche_duo(n, T1, T2, score);
+            affiche_duo(n, T1, T2, score);// Affichage des deux tableaux
         }
     }
 }
 
 void normal(int n, int T1[n][n], int *score){
-    int a = 1 ;
+    /*Fonction principale du mode normal*/
+    int jouer = 1 ;
     char dir;
     char rep= 'd';
+    //Tentative de récupération du fichier de sauvegarde
     FILE * fichier = NULL ;
     fichier = fopen("Save.txt", "r");
     fseek(fichier,0, SEEK_END);
     int s = ftell(fichier);
     fclose(fichier);
-    if (s != 0) {
-        fichier = fopen("Save.txt", "r");
-        fscanf(fichier , "%d",  &n );
-        fclose(fichier);
-        while (rep != 'o' && rep != 'n'){
+    if (s != 0) { // Si le fichier de sauveagrde existe
+        //fichier = fopen("Save.txt", "r");
+        //fclose(fichier);
+        while (rep != 'o' && rep != 'n'){// Demande au joueur si il veut reprendre son fichier de sauvegarde.
             printf("Vous avez une partie de sauvegarder, voulez la continuer ?(oui/non)");
             scanf("%c", &rep);
             tolower(rep);
@@ -291,25 +308,28 @@ void normal(int n, int T1[n][n], int *score){
 
         }
         if (rep =='o'){
-            Lecture(n, T1, score);
+            Lecture(n, T1, score); // Si oui récupérons les données du fichier de sauvegarde et on remplis le tableaux
         }
         else{
-            creationTab(n, T1);
+            creationTab(n, T1);// Sinon on créer un nouveau tableaux
+            printf("%d", n);
             add_case(n, T1, score);
         }
     }else{
-        creationTab(n, T1);
+        creationTab(n, T1);// SI aucun fichier de sauvarge détécté.
         add_case(n, T1, score);
     }
 
     affiche(n, T1, score);
-    while (a) {
+    while (jouer) {
         printf("dans quel direction voulez vous aller ? (l/r/u/d) ou 'a' pour quitter");
         scanf("%c", &dir);
         fflush(stdin);
         tolower(dir);
-        if (dir == 'a') { a = 0;
-            Sauvegarde(n, T1 , *score ); }
+        if (dir == 'a') {
+            jouer = 0;
+            Sauvegarde(n, T1 , *score );// Sauvegarde du tableaux dans un fichier en cas d'arret.
+        }
         else {
             fusion(n, T1, dir);
             move(n, T1, dir);
@@ -321,31 +341,40 @@ void normal(int n, int T1[n][n], int *score){
 }
 
 void puzzle(int *score){
-    int a = 1;
-
+    /*Fonction principale du mode puzzle */
+    int jouer = 1;
     char dir;
-    FILE* f = fopen("./../puzzleStorage/puzzle1.txt", "r");
+
+
+    //Ouverture du fichier puzzle.
+    FILE* f = fopen("./puzzleOne.txt", "r");
+
     if (f != NULL) {
+        //Récupération de la taille du tableaux
+
+
         char buffer[256];
         fgets(buffer, 256, f);
-        strtok(buffer, "|");
-        int n = *strtok(NULL, "|") - '0';
+        strtok(buffer, " ");
+
+        int n = atoi(strtok(NULL, " "));
+
         int T1[n][n];
 
 
 
         fgets(buffer, 256, f);
-
         for (int i = 0; i < n; i++) {
+            //Récupération de chaque ligne du tableaux
             char * token = strtok(buffer, "|");
-            if (atoi(token) == -1) {
+            if (atoi(token) == -1) {// La fonction atoi() convertis un char en Int -> ('7' -> 7)
                 T1[i][0] = -1;
             } else {
                 T1[i][0] = atoi(token);
             }
 
             for (int y = 1; y < n; y++) {
-
+                // Récupération de chaque colonne du tableaux
                 token = strtok(NULL, "|");
 
                 if (atoi(token) == -1) {
@@ -360,12 +389,15 @@ void puzzle(int *score){
 
         fclose(f);
         affiche(n, T1, score);
-        while (a) {
-            printf("dans quel direction voulez vous aller ? (l/r/u/d) ou 'a' pour quitter");
+        while (jouer) {
+            //Similaire à la fonction normal() du programme
+            printf("dans quel direction voulez vous aller ? (q/d/z/s) ou 'a' pour quitter");
             scanf("%c", &dir);
             fflush(stdin);
             tolower(dir);
-            if (dir == 'a') { a = 0; }
+            if (dir == 'a') {
+                jouer = 0;
+            }
             else {
                 fusion(n, T1, dir);
                 move(n, T1, dir);
@@ -378,8 +410,9 @@ void puzzle(int *score){
 }
 
 void Sauvegarde(int n , int T[n][n], int Score ){
+    /*Sauvegarde le contenu du tableaux dans un fichier texte*/
     FILE* fichier = NULL;
-    fichier = fopen("Save.txt","w" );
+    fichier = fopen("Save.txt","w" );// Ecriture dans le fichier de sauvegarde/Création du fichier de sauvegarde si il n'existe pas
     fprintf(fichier, "%d ", n );
     for(int i =0; i<n ; i++){
         for (int y = 0 ; y<n ; y++){
@@ -391,6 +424,7 @@ void Sauvegarde(int n , int T[n][n], int Score ){
     fclose(fichier );
 }
 void Lecture(int n, int T[n][n] , int* Score){
+    /*Lis le contenu d'un fichier de sauvegarde pour le transférer dans un tableaux*/
     FILE* fichier = NULL;
 
     fichier = fopen("Save.txt", "r");
@@ -411,6 +445,8 @@ void Lecture(int n, int T[n][n] , int* Score){
 
 
 void jeu(int n, int mode) {
+    /* Prend en paramètre un Integer qui décide du mode de jeu choisis*/
+
     int score = 0, T1[n][n], T2[n][n];
     if (mode == 1) {
         normal(n, T1, &score);
