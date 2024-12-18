146ce48788d4416b23a944d2afab05f66e229d07
Suhayb <98946068+Poimurgh@users.noreply.github.com>
2024-12-18 11:54:19 +0100

    Update main.c
    on ne demande plus si vous voulez jouer lorsque vous lancez le programme

--- src/main.c
+++ src/main.c
@@ -5,38 +5,25 @@
 #include <time.h>
 
 int main(){
-    char rep='d';
     srand(time(0));
-
-    while (rep!='o' && rep!='n'){ // Demande au joueur si il veut lance une partie
-        printf("Voulez-vous jouer ? (O/N)");
-        scanf("%c",&rep);
-        rep=tolower(rep);
-        fflush(stdin);
-        if(rep!='o' && rep!='n'){
+    int n=0, mode;
+    while (mode<1||mode>3) { // Si le joueur accepte de jouer on lui demande le mode de jeu qu'il veut
+        printf("Veuillez choisir la mode de Jeu \n1 : mode Classique\n2 : mode Duo\n3 : mode Puzzle \n");
+        scanf("%d",&mode);
+        if (mode<1||mode>3){
             printf("Saisie incorrect, Veuillez resaisir ! \n");
         }
     }
-    if (rep=='o'){
-        int n=0, mode;
-        while (mode<1||mode>3) { // Si le joueur accepte de jouer on lui demande le mode de jeu qu'il veut
-            printf("Veuillez choisir la mode de Jeu \n1 : mode Classique\n2 : mode Duo\n3 : mode Puzzle \n");
-            scanf("%d",&mode);
-            if (mode<1||mode>3){
+    if(mode!=3){// Si le mode de jeu n'est pas le puzzle alors on lui demande la taille du tableaux.
+        while (n<4||n>9) {
+            printf("Veuillez choisir la taille du tableau (entre 4 et 9) :");
+            scanf("%d", &n);
+            if (n < 4 || n > 9) {
                 printf("Saisie incorrect, Veuillez resaisir ! \n");
             }
         }
-        if(mode!=3){// Si le mode de jeu n'est pas le puzzle alors on lui demande la taille du tableaux.
-            while (n<4||n>9) {
-                printf("Veuillez choisir la taille du tableau (entre 4 et 9) :");
-                scanf("%d", &n);
-                if (n < 4 || n > 9) {
-                    printf("Saisie incorrect, Veuillez resaisir ! \n");
-                }
-            }
-        }
-        fflush(stdin);
-        jeu(n,mode);
     }
+    fflush(stdin);
+    jeu(n,mode);
 }
 
